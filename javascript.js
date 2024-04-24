1c2529234219917bf16dd6ee44c2ecab7c85dfb3
LP <lukcola10@gmail.com>
2024-04-24 17:53:46 +0200

    drugi push

---
+++ javascript.js
@@ -0,0 +1,36 @@
+/*
+var brojcaniNiz = [1, 2, , 4, 5, "MOJE IME", 7];
+var suma = 0;
+
+for (i = 0; i < brojcaniNiz.length; i++) {
+    if(brojcaniNiz[i] === undefined){
+        continue;
+    }
+    suma += brojcaniNiz[i];
+}
+
+console.log(suma);
+
+
+
+for (i = 0; i < brojcaniNiz.length; i++) {
+    console.log(typeof (brojcaniNiz[i]));
+    if (typeof (brojcaniNiz[i]) != "number") {
+        continue;
+    }
+    console.log(i + ". je tip: " + typeof (brojcaniNiz[i]))
+    suma += brojcaniNiz[i];
+}
+
+console.log(suma);
+
+
+var matrix = [[1, 2, 3], ["John", "Jane", "Mike"]];
+
+for (var i = 0; i < matrix.length; i++) {
+    for (var j = 0; j < matrix[i].length; j++) {
+        console.log("matrix[" + i + "][" + j + "] = " + matrix[i][j]);
+    }
+}
+*/
+
